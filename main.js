b2fe3b204229257f69576441fa72acc4a6493619
Eric Seitz <EricESeitz@gmail.com>
2019-02-07 21:02:41 -0600

    Testing 2D array creation, calling

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-//Creates n-dimensional array. Source below
-        //https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
-
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Test of main.js implementation of code';
 
+//Creates n-dimensional array. Source below
+//https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+function createArray(length) {
+    var arr = new Array(length || 0),
+        i = length;
+
+    if (arguments.length > 1) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+
+    return arr;
+}//end of n-dimensional arr
+
 function TestFunction(x, y)
 {
-    return x + y;
+    alert('Coordinates: (' + x + ', ' + y + ')');
 }
