816713b7b6525013fc73eb414eb9a0cf9cf4993f
Eric Seitz <EricESeitz@gmail.com>
2019-02-07 20:23:16 -0600

    Prototype coordinate passing to main.js function

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+//Creates n-dimensional array. Source below
+        //https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
+
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'Test of main.js implementation of code';
+
+function TestFunction(x, y)
+{
+    return x + y;
+}
