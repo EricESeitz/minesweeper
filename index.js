8bf173656dc210aef2413f965e496943868cc139
Lauren Stephenson <compscilauren@gmail.com>
2019-02-07 22:15:50 -0600

    Add drawSquares function to display game grid

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Minesweeper");
+//let size = prompt('Input a size for the grid');
+drawSquares();
+function drawSquares(square) {
+  $("#squareContainer").empty();
+  for (let row = 0; row < 8; row++) {
+    let rowElement = $("<div>");
+    rowElement.addClass("row");
+    for (let column = 0; column < 8; column++) {
+      let squareElement = $("<div>");
+      squareElement.addClass("square");
+      rowElement.append(squareElement);
+    }
+    $("#squareContainer").append(rowElement);
+  }
+}
