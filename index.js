4f037cf7e78acae93441da7b06c539592ec82531
Lauren Stephenson <compscilauren@gmail.com>
2019-02-07 22:29:49 -0600

    Assign each square a unique id

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let gridSize = prompt("Input a size for the grid");
 drawSquares();
 function drawSquares(square) {
   $("#squareContainer").empty();
+  let id = 0;
   for (let row = 0; row < gridSize; row++) {
     let rowElement = $("<div>");
     rowElement.addClass("row");
     for (let column = 0; column < gridSize; column++) {
       let squareElement = $("<div>");
       squareElement.addClass("square");
+      squareElement.attr("id", id);
+      id++;
       rowElement.append(squareElement);
     }
     $("#squareContainer").append(rowElement);
