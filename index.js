469c517f18bacb1eea8aa5f99d456639fda319a0
Lauren Stephenson <compscilauren@gmail.com>
2019-02-07 22:21:14 -0600

    Allow user to dictate grid size

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//let size = prompt('Input a size for the grid');
+let gridSize = prompt("Input a size for the grid");
 drawSquares();
 function drawSquares(square) {
   $("#squareContainer").empty();
-  for (let row = 0; row < 8; row++) {
+  for (let row = 0; row < gridSize; row++) {
     let rowElement = $("<div>");
     rowElement.addClass("row");
-    for (let column = 0; column < 8; column++) {
+    for (let column = 0; column < gridSize; column++) {
       let squareElement = $("<div>");
       squareElement.addClass("square");
       rowElement.append(squareElement);
