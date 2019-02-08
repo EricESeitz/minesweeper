f09469b62fcc0aed83c5c41ba3c32215c992daba
Lauren Stephenson <compscilauren@gmail.com>
2019-02-07 22:15:50 -0600

    Implement grid size requirements, add x y coords, add drawSquares function to display game grid

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Minesweeper");
+let validGridSize = false;
+let gridSize = parseInt(Number(prompt("Input a size for the grid")));
+
+while (validGridSize == false) {  
+
+  if (gridSize < 2 || gridSize % 1 !== 0) {
+    alert("Invalid input. Enter an integer between 2 and 100");
+    gridSize = parseInt(Number(prompt("Input a size for the grid")));
+  }
+  else if (gridSize > 100 || gridSize % 1 !== 0) {
+    alert("Invalid input. Enter an integer between 2 and 100");
+    gridSize = parseInt(Number(prompt("Input a size for the grid")));
+  }
+  else {
+    validGridSize = true;
+  }
+}
+
+drawSquares();
+function drawSquares(square) {
+  $("#squareContainer").empty();
+  for (let row = 0; row < gridSize; row++) {
+    let rowElement = $("<div>");
+    rowElement.addClass("row");
+    for (let column = 0; column < gridSize; column++) {
+      let squareElement = $("<div>");
+      squareElement.addClass("square");
+      squareElement.attr("x_coordinate", row);
+      squareElement.attr("y_coordinate", column);
+      rowElement.append(squareElement);
+    }
+    $("#squareContainer").append(rowElement);
+  }
+}
