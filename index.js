b030c1db202a88548a85f25ebf5769107188297f
Eric Seitz <EricESeitz@gmail.com>
2019-02-13 22:53:55 -0600

    Fixed squareProperties, now correctly unique for each square (no longer shared across all squares). Testing with ClickTimes successful

--- index.js
+++ index.js
@@ -50,18 +50,27 @@ function arrayFillerRandom()
 }
 */
 
+//For storing values in the 2D array, used to create unique instance 'objects'. Can be called on by arr[x][y].<property>
+function squareProperties(isClicked, isBomb, isFlagged, numNeighborMines, testingClickTimes)
+{
+  this.isClicked = isClicked;
+  this.isBomb = isBomb;
+  this.isFlagged = isFlagged;
+  this.numNeighborMines = numNeighborMines;
+  this.testingClickTimes = testingClickTimes;
+}
 //fills array with "squareProperties" object, will be used to store properties
 //of the grid squares (i.e isClicked - if square has been clicked before. isBomb - if it is a mine space)
 arrayFiller();
 function arrayFiller()
 {
-  var squareProperties = {isClicked:0, isBomb:0, isFlagged:0, numNeighborMines:0, testingClickTimes:0};
+  
 
   for (let i = 0; i < gridSize; i++)
   {
     for (let k = 0; k < gridSize; k++)
     {
-    arr[i][k] = squareProperties;
+    arr[i][k] = new squareProperties(0, 0, 0, 0, 0);  //initial assignment, all properties 0
     }
   }
 }
@@ -82,4 +91,18 @@ function drawSquares(square) {
     }
     $("#squareContainer").append(rowElement);
   }
+}
+
+$(".square").on("click", function() {
+  const elementClicked = $(this);
+  const xPos = elementClicked.attr("data-x-coordinate");
+  const yPos = elementClicked.attr("data-y-coordinate");
+  TestFunction(xPos, yPos);
+});
+
+//testing function for 2D arr, should display (x, y) coordinates on grid click, plus number of times a spesific square has been clicked on
+function TestFunction(x, y)
+{
+  arr[x][y].testingClickTimes = arr[x][y].testingClickTimes + 1;  //increments testingClickTimes up by one for each click
+    alert('Coordinates: (' + x + ', ' + y + ')' + "\nNum Clicked: " + arr[x][y].testingClickTimes);
 }
