4dfcb84e0c0cdabc7cf875efa4c879d399e50721
Eric Seitz <EricESeitz@gmail.com>
2019-02-15 08:00:05 -0600

    Colour changes and minor bugfixes ('var' to 'let')

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ while (validGridSize == false) {
 //Creates n-dimensional array. Source below
 //https://stackoverflow.com/questions/966225/how-can-i-create-a-two-dimensional-array-in-javascript/966938#966938
 function createArray(length) {
-  var arr = new Array(length || 0),
+  let arr = new Array(length || 0),
       i = length;
 
   if (arguments.length > 1) {
-      var args = Array.prototype.slice.call(arguments, 1);
+      let args = Array.prototype.slice.call(arguments, 1);
       while(i--) arr[length-1 - i] = createArray.apply(this, args);
   }
   
@@ -31,7 +31,7 @@ function createArray(length) {
 }//end of n-dimensional arr
 
 //Creates Global 2D array matching user-defined grid size
-var arr = createArray(gridSize, gridSize);
+let arr = createArray(gridSize, gridSize);
 
 //Testing function, fills the array with random numbers
 //to test 'connection' between clickable grid and array
@@ -64,8 +64,6 @@ function squareProperties(isClicked, isBomb, isFlagged, numNeighborMines, testin
 arrayFiller();
 function arrayFiller()
 {
-  
-
   for (let i = 0; i < gridSize; i++)
   {
     for (let k = 0; k < gridSize; k++)
@@ -78,7 +76,6 @@ function arrayFiller()
 drawSquares();
 function drawSquares(square) {
   $("#squareContainer").empty();
-  let id = 0;
   for (let row = 0; row < gridSize; row++) {
     let rowElement = $("<div>");
     rowElement.addClass("row");
@@ -97,11 +94,11 @@ $(".square").on("click", function() {
   const elementClicked = $(this);
   const xPos = elementClicked.attr("data-x-coordinate");
   const yPos = elementClicked.attr("data-y-coordinate");
-  TestFunction(xPos, yPos);
+  onClicked(xPos, yPos);
 });
 
-//testing function for 2D arr, should display (x, y) coordinates on grid click, plus number of times a spesific square has been clicked on
-function TestFunction(x, y)
+//on user clicking on a grid square. **Just For testing: should display (x, y) coordinates on grid click, plus number of times a spesific square has been clicked on
+function onClicked(x, y)
 {
   arr[x][y].testingClickTimes = arr[x][y].testingClickTimes + 1;  //increments testingClickTimes up by one for each click
     alert('Coordinates: (' + x + ', ' + y + ')' + "\nNum Clicked: " + arr[x][y].testingClickTimes);
